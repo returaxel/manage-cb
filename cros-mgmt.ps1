e0630c7401334413aec14c9f73d68b165f551be1
returaxel <48722741+returaxel@users.noreply.github.com>
2020-01-22 10:27:41 +0100

    Update cros-mgmt.ps1

--- cros-mgmt.ps1
+++ cros-mgmt.ps1
@@ -10,8 +10,6 @@
     Device serial number.
 .PARAMETER optionalInfo
     Accepts optional GAM commands, ex; allFields, basic, OrgUnitPath.
-.PARAMETER logSavePath
-    Specify a new location, ex: "C:\temp", or leave blank. Default save location is script root. 
 .INPUTS
     None
 .OUTPUTS
@@ -34,43 +32,12 @@ param(
     [ValidateSet("reenable", "deprovision", "disable", "info")][string]$action,
     [Parameter(Position=2, Mandatory=$true)][string]$serialNumber,
     [Parameter(Position=3, Mandatory=$false,ValueFromRemainingArguments=$true)][psobject[]]$optionalInfo
-    #[Parameter(Position=4, Mandatory=$false)][string]$logSavePath
 )
 
 #Set Error Action to Silently Continue
 $ErrorActionPreference = "SilentlyContinue"
 
 #-----------------------------------------------------------[Functions]------------------------------------------------------------
-
-function Set-Log{
-    param (
-        [Parameter(Mandatory=$false)][string]$logSavePath,
-        [Parameter(Mandatory=$true)][string]$action,
-        [Parameter(Mandatory=$true)][string]$serialNumber
-    )
-
-    [string]$dateTime = Get-Date                                # Set date
-    [string]$deviceInfo = "$dateTime, $serialNumber, $action"   # Set logging info
-
-    if ($logSavePath.gettype().name -eq 'String' -and $logSavePath.length -eq 0) {
-        $logSavePath = $PSScriptRoot
-    } 
-    elseif ((Test-Path $logSavePath) -ne $true) {
-        mkdir > $null -Path $logSavePath
-    }
-    try {
-        $deviceInfo | Out-File -FilePath $logSavePath\cros-log.txt -Append
-    }catch{
-        Write-Host "Could not write to $logSavePath\cros-log.txt" -f Yellow
-    } 
-}
-
-function Get-SerialNumber {
-    param (
-        [Parameter(Mandatory=$true)]$serialNumber
-    )
-        gam cros_sn $serialNumber print
-}
 
 function Set-State{
     param (
@@ -79,18 +46,16 @@ function Set-State{
         [Parameter(Mandatory=$false)][psobject[]]$optionalInfo
     )
     switch ($action) {
-        deprovision {gam update cros $sN action deprovision_retiring_device acknowledge_device_touch_requirement}
-        disable     {gam update cros $sN action disable}
-        reenable    {gam update cros $sN action reenable}
-        info        {gam info cros $sN fields serialNumber status $optionalInfo}
+        deprovision {gam update cros query $serialNumber action deprovision_retiring_device acknowledge_device_touch_requirement}
+        disable     {gam update cros query $serialNumber action disable}
+        reenable    {gam update cros query $serialNumber action reenable}
+        info        {gam info cros query $serialNumber fields serialNumber status $optionalInfo}
         default     {break}
     }
 
 }
 
-#-----------------------------------------------------------[Execution]------------------------------------------------------------
- 
-#Set-Log $logSavePath $action $serialNumber 
-$sN = Get-SerialNumber $serialNumber
-Set-State $action $sN $optionalInfo
+#-----------------------------------------------------------[Execution]------------------------------------------------------------ne
+
+Set-State $action $serialNumber $optionalInfo
 
